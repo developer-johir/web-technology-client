a53fdb26601adf83cce052611a9130922535dfcc
Jahirul Islam <jahirulislam@Shafiqs-MacBook-Pro-2.local>
2022-10-30 09:59:06 +0600

    Footer complete

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
+        <div className='bg-dark text-white text-center py-2'>
             <p>Copyright coming soon</p>
         </div>
     );
